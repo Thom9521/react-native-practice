56e5e39f6481593c017f839d590de6ad475e1a8f
Thomas Christensen <thom9521@edu.easj.dk>
2020-11-23 12:44:56 +0100

    Comment

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ import ToastComponent from './components/ToastComponent';
 import ParentComponent from './components/ParentComponent';
 import {name as appName} from './app.json';
 
-// Switch ToastComponent to BasicComponents on line 12 to test the BasicComponents component
+// Switch between the components you want to see on line 13.
 
 AppRegistry.registerComponent(appName, () => ParentComponent);
